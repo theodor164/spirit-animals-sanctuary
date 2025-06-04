063f1b41366f2121ce4edf4a69f4b45032e05d3f
theodor164 <tanase.ted.164@gmail.com>
2025-06-04 22:01:22 +0300

    add about us homepage section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { AboutUsComponent } from '../../sections/about-us/about-us.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AboutUsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
---
+++ src/app/sections/about-us/about-us.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-about-us',
+  imports: [NgOptimizedImage],
+  templateUrl: './about-us.component.html',
+  styleUrl: './about-us.component.css',
+})
+export class AboutUsComponent {}

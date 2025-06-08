97ce467db1826e7aa8d4896a1459100d8b4d0a58
theodor164 <tanase.ted.164@gmail.com>
2025-06-08 17:04:06 +0300

    add animal list basic page

--- src/app/pages/animals/animal-list/animal-list.component.ts
+++ src/app/pages/animals/animal-list/animal-list.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf, NgOptimizedImage } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-animal-list',
-  imports: [],
+  imports: [NgOptimizedImage, RouterModule, NgFor, NgIf],
   templateUrl: './animal-list.component.html',
-  styleUrl: './animal-list.component.css'
+  styleUrl: './animal-list.component.css',
 })
 export class AnimalListComponent {
+  animals = [
+    {
+      name: 'Caprita',
+      thumbnail:
+        'https://spiritanimals.ro/wp-content/uploads/2023/06/Caprita-Icon@4x.png',
+      full: 'https://spiritanimals.ro/wp-content/uploads/2023/08/363427131_672053981634517_6459136923972407307_n-2.webp',
+    },
+    {
+      name: 'Mieluț',
+      thumbnail:
+        'https://spiritanimals.ro/wp-content/uploads/2023/06/Love-2@4x.png',
+      full: 'https://spiritanimals.ro/wp-content/uploads/2023/06/328737775_507508738241035_7029562333189319196_n.jpg',
+    },
+    // Add more animals here
+  ];
 
+  currentIndex: number = 0;
+  lightboxOpen: boolean = false;
+
+  openLightbox(index: number) {
+    this.currentIndex = index;
+    this.lightboxOpen = true;
+  }
+
+  closeLightbox(event?: MouseEvent) {
+    if (
+      event &&
+      (event.target as HTMLElement).classList.contains('lightbox-content')
+    )
+      return;
+    this.lightboxOpen = false;
+  }
+
+  prevImage() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.animals.length) % this.animals.length;
+  }
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.animals.length;
+  }
 }

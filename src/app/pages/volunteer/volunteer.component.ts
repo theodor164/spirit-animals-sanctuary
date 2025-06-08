8dd940e47acb2485ab8708b356970b1ff35e6595
theodor164 <tanase.ted.164@gmail.com>
2025-06-08 17:08:57 +0300

    add basic volunteer page

--- src/app/pages/volunteer/volunteer.component.ts
+++ src/app/pages/volunteer/volunteer.component.ts
@@ -1,11 +1,18 @@
+import { NgFor, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-volunteer',
-  imports: [],
+  imports: [NgOptimizedImage, RouterModule, NgFor],
   templateUrl: './volunteer.component.html',
-  styleUrl: './volunteer.component.css'
+  styleUrl: './volunteer.component.css',
 })
 export class VolunteerComponent {
-
+  images: string[] = [
+    'https://spiritanimals.ro/wp-content/uploads/2023/06/Caprita-Icon@4x.png',
+    'https://spiritanimals.ro/wp-content/uploads/2023/06/Love-2@4x.png',
+    'https://spiritanimals.ro/wp-content/uploads/2023/06/Caprita-Icon@4x.png',
+    'https://spiritanimals.ro/wp-content/uploads/2023/06/Caprita-Icon@4x.png',
+  ];
 }

18980a39233ee353b366027a62178824c6ab36c3
theodor164 <tanase.ted.164@gmail.com>
2025-06-08 16:56:32 +0300

    add basic donate page, basic our story page

--- src/app/pages/donate/donate.component.ts
+++ src/app/pages/donate/donate.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-donate',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './donate.component.html',
   styleUrl: './donate.component.css'
 })
--- src/app/pages/our-story/our-story.component.ts
+++ src/app/pages/our-story/our-story.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-our-story',
-  imports: [],
+  imports: [NgOptimizedImage, RouterModule],
   templateUrl: './our-story.component.html',
-  styleUrl: './our-story.component.css'
+  styleUrl: './our-story.component.css',
 })
-export class OurStoryComponent {
-
-}
+export class OurStoryComponent {}

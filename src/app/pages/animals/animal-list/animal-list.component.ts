53415c30b0b632a32fcd75b13e893fd9519931d4
theodor164 <tanase.ted.164@gmail.com>
2025-06-08 17:20:30 +0300

    add basic adoption page

--- src/app/pages/animals/animal-list/animal-list.component.ts
+++ src/app/pages/animals/animal-list/animal-list.component.ts
@@ -13,13 +13,13 @@ export class AnimalListComponent {
     {
       name: 'Caprita',
       thumbnail:
-        'https://spiritanimals.ro/wp-content/uploads/2023/06/Caprita-Icon@4x.png',
+        'https://spiritanimals.ro/wp-content/uploads/2023/08/363427131_672053981634517_6459136923972407307_n-2.webp',
       full: 'https://spiritanimals.ro/wp-content/uploads/2023/08/363427131_672053981634517_6459136923972407307_n-2.webp',
     },
     {
-      name: 'Mieluț',
+      name: 'Calut',
       thumbnail:
-        'https://spiritanimals.ro/wp-content/uploads/2023/06/Love-2@4x.png',
+        'https://spiritanimals.ro/wp-content/uploads/2023/06/328737775_507508738241035_7029562333189319196_n.jpg',
       full: 'https://spiritanimals.ro/wp-content/uploads/2023/06/328737775_507508738241035_7029562333189319196_n.jpg',
     },
     // Add more animals here
--- src/app/pages/volunteer/volunteer.component.ts
+++ src/app/pages/volunteer/volunteer.component.ts
@@ -10,9 +10,9 @@ import { RouterModule } from '@angular/router';
 })
 export class VolunteerComponent {
   images: string[] = [
-    'https://spiritanimals.ro/wp-content/uploads/2023/06/Caprita-Icon@4x.png',
-    'https://spiritanimals.ro/wp-content/uploads/2023/06/Love-2@4x.png',
-    'https://spiritanimals.ro/wp-content/uploads/2023/06/Caprita-Icon@4x.png',
-    'https://spiritanimals.ro/wp-content/uploads/2023/06/Caprita-Icon@4x.png',
+    'https://spiritanimals.ro/wp-content/uploads/2023/06/received_109045552191583-2.webp',
+    'https://spiritanimals.ro/wp-content/uploads/2023/06/received_948878836428957-1024x683-290x379.jpg',
+    'https://spiritanimals.ro/wp-content/uploads/2023/06/332489732_589908163153933_8834787529273301537_n.webp',
+    'https://spiritanimals.ro/wp-content/uploads/2023/06/350944195_1002827670709851_4848279126552609894_n-290x370.webp',
   ];
 }

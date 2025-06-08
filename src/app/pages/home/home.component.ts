6cc9d218fc3d0938045ca3f9c99a9d3a055ec59b
theodor164 <tanase.ted.164@gmail.com>
2025-06-08 15:06:22 +0300

    add visit us section on homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { AboutUsComponent } from '../../sections/about-us/about-us.component';
 import { OurImpactComponent } from "../../sections/our-impact/our-impact.component";
 import { DonateSectionComponent } from '../../sections/donate-section/donate-section.component';
 import { VolunteerSectionComponent } from "../../sections/volunteer-section/volunteer-section.component";
+import { VisitUsComponent } from "../../sections/visit-us/visit-us.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AboutUsComponent, OurImpactComponent, DonateSectionComponent, VolunteerSectionComponent],
+  imports: [AboutUsComponent, OurImpactComponent, DonateSectionComponent, VolunteerSectionComponent, VisitUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
---
+++ src/app/sections/visit-us/visit-us.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visit-us',
+  imports: [],
+  templateUrl: './visit-us.component.html',
+  styleUrl: './visit-us.component.css'
+})
+export class VisitUsComponent {
+
+}

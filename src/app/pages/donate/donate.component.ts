5c00892a4676878f2009a34056d24ffecd53e3af
theodor164 <tanase.ted.164@gmail.com>
2025-06-09 16:07:36 +0300

    add donations ranking

--- src/app/pages/donate/donate.component.ts
+++ src/app/pages/donate/donate.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-donate',
-  imports: [RouterModule],
+  imports: [RouterModule, NgIf, NgFor, CommonModule],
   templateUrl: './donate.component.html',
-  styleUrl: './donate.component.css'
+  styleUrl: './donate.component.css',
 })
 export class DonateComponent {
+  ranking: { name: string; total: number }[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<{ name: string; total: number }[]>(
+        'http://localhost:3000/api/donations/ranking'
+      )
+      .subscribe({
+        next: (data) => (this.ranking = data),
+        error: (error) => console.error('Failed to load ranking', error),
+      });
+  }
 }

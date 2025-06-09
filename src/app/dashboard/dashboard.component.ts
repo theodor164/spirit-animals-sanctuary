2e45fd4e4a6ca10269e6a5232676642f57f643b3
theodor164 <tanase.ted.164@gmail.com>
2025-06-09 14:15:47 +0300

    add basic donation inside user dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { NgFor, NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 
 interface User {
   id: number;
@@ -18,7 +18,7 @@ interface Donation {
 
 @Component({
   selector: 'app-dashboard',
-  imports: [NgIf, NgFor, CommonModule],
+  imports: [NgIf, NgFor, CommonModule, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
@@ -26,8 +26,35 @@ export class DashboardComponent {
   message = '';
   user: User | null = null;
   donations: Donation[] = [];
+  donationAmount: number = 0;
+  showDonationForm: boolean = false;
 
   constructor(private http: HttpClient) {}
+
+  toggleDonationForm() {
+    this.showDonationForm = !this.showDonationForm;
+  }
+
+  submitDonation() {
+    if (this.donationAmount <= 0) {
+      return;
+    }
+
+    this.http.post('http://localhost:3000/api/donations', {
+      amount: this.donationAmount,
+    }).subscribe({
+      next: () => {
+        this.message = 'Thamk you for your donation!';
+        this.donationAmount = 0;
+        this.showDonationForm = false;
+        // Optionally, you can refresh the donations list
+        this.ngOnInit();
+      },
+      error: (error) => {
+        this.message = 'Donation failed: ' + error.message;
+      }
+    });
+  }
 
   ngOnInit() {
     this.http
